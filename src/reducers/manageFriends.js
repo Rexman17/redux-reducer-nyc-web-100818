ead8712a7081decfc12dec7cb181ba9e45d3fb50
Rexman17 <a.stone1794@gmail.com>
2019-01-02 15:35:58 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return { friends: [...state.friends, action.friend] }
+    case 'REMOVE_FRIEND':
+      const friends = state.friends.filter(f => f.id !== action.id)
+      return ({friends: friends})
+    default:
+      return state;
+  }
 }
+//
+// var wishes = this.state.wishes.filter(wish => wish.id !== id)
+//     this.setState({wishes: wishes})
